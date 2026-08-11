c4cae40355542c01e71a20d26e51815573f2b2ac
Carlos Henrique <carlosouza.dev@gmail.com>
2026-08-11 16:10:28 -0300

    Implementa métodos de CRUD

--- src/main/java/com/github/carlosouza_dev/libraryapi/controller/AutorController.java
+++ src/main/java/com/github/carlosouza_dev/libraryapi/controller/AutorController.java
@@ -3,12 +3,12 @@ package com.github.carlosouza_dev.libraryapi.controller;
 import com.github.carlosouza_dev.libraryapi.controller.dto.AutorDTO;
 import com.github.carlosouza_dev.libraryapi.model.Autor;
 import com.github.carlosouza_dev.libraryapi.service.AutorService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -37,15 +37,77 @@ public class AutorController {
         return ResponseEntity.created(uri).build();
     }
 
+    //GET http://localhost:8080/autores/ass-22ljklafa-asdfa
     @GetMapping("/{id}")
-    public ResponseEntity<Autor> buscar(@PathVariable UUID id){
+    public ResponseEntity<AutorDTO> buscar(@PathVariable UUID id){
         Optional<Autor> optAutor = service.buscar(id);
 
         if(optAutor.isPresent()){
-            return ResponseEntity.ok(optAutor.get());
+            Autor autor = optAutor.get();
+
+            AutorDTO autorDTO = new AutorDTO(
+                    autor.getId(),
+                    autor.getNome(),
+                    autor.getDataNascimento(),
+                    autor.getNacionalidade()
+            );
+
+            return ResponseEntity.ok(autorDTO);
         }
 
         return ResponseEntity.notFound().build();
+    }
 
+    @DeleteMapping("/{id}")
+     public ResponseEntity<Void> deletar(@PathVariable UUID id){
+        Optional<Autor> optAutor = service.buscar(id);
+
+        if (optAutor.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        service.deletar(optAutor.get());
+
+        return ResponseEntity.noContent().build();
+    }
+
+    //GET http://localhost:8080/autores?nome=loscar
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> filtrar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade
+    ){
+        List<Autor> listAutores = service.filtrar(nome, nacionalidade);
+
+        List<AutorDTO> listAutoresDTO = listAutores.stream()
+                .map(autor -> {
+                    return new AutorDTO(
+                            autor.getId(),
+                            autor.getNome(),
+                            autor.getDataNascimento(),
+                            autor.getNacionalidade()
+                    );
+                }).toList();
+
+        return ResponseEntity.ok(listAutoresDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizar(
+            @PathVariable UUID id,
+            @RequestBody AutorDTO dto
+    ){
+        Optional<Autor> optAutor = service.buscar(id);
+
+        if(optAutor.isEmpty()) return ResponseEntity.notFound().build();
+
+        Autor autor = optAutor.get();
+        autor.setNome(dto.nome());
+        autor.setDataNascimento(dto.dataNascimento());
+        autor.setNacionalidade(dto.nacionalidade());
+
+        service.atualizar(autor);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/github/carlosouza_dev/libraryapi/service/AutorService.java
+++ src/main/java/com/github/carlosouza_dev/libraryapi/service/AutorService.java
@@ -2,9 +2,9 @@ package com.github.carlosouza_dev.libraryapi.service;
 
 import com.github.carlosouza_dev.libraryapi.model.Autor;
 import com.github.carlosouza_dev.libraryapi.repository.AutorRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -21,7 +21,33 @@ public class AutorService {
         return repository.save(autor);
     }
 
+    public void atualizar(Autor autor){
+        if (autor.getId() == null) throw new IllegalArgumentException("Autor precisa ter id para atualizar");
+
+        repository.save(autor);
+    }
+
     public Optional<Autor> buscar(UUID id) {
         return repository.findById(id);
+    }
+
+    public void deletar(Autor autor){
+        repository.delete(autor);
+    }
+
+    public List<Autor> filtrar(String nome, String nacionalidade){
+        if (nome != null && nacionalidade != null) {
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+
+        if (nome != null) {
+            return repository.findByNome(nome);
+        }
+
+        if (nacionalidade != null){
+            return repository.findByNacionalidade(nacionalidade);
+        }
+
+        return repository.findAll();
     }
 }
