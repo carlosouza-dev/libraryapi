f3b2e1f81a561ae9f9f271f89cddd48c69e0faf8
Carlos Henrique <carlosouza.dev@gmail.com>
2026-08-30 21:40:08 -0300

    Mapeia todas as excecões tratadas e não tratadas

--- src/main/java/com/github/carlosouzadev/libraryapi/controller/common/GlobalExceptionHandler.java
+++ src/main/java/com/github/carlosouzadev/libraryapi/controller/common/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package com.github.carlosouzadev.libraryapi.controller.common;
 
 import com.github.carlosouzadev.libraryapi.controller.dto.ErroCampo;
 import com.github.carlosouzadev.libraryapi.controller.dto.ErroResposta;
+import com.github.carlosouzadev.libraryapi.exception.OperacaoNaoPermitidaException;
+import com.github.carlosouzadev.libraryapi.exception.RegistroDuplicadoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -30,5 +33,31 @@ public class GlobalExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).body(erroResposta);
+    }
+
+    // Exceções de regras de negócios
+    @ExceptionHandler(RegistroDuplicadoException.class)
+    public ResponseEntity<ErroResposta> handleRegistroDuplicadoException(RegistroDuplicadoException e){
+        ErroResposta erro = ErroResposta.conflito(e.getMessage());
+
+        return ResponseEntity.status(erro.status()).body(erro);
+    }
+
+    @ExceptionHandler(OperacaoNaoPermitidaException.class)
+    public ResponseEntity<ErroResposta> handleOperacaoNaoPermitidaException(OperacaoNaoPermitidaException e){
+        ErroResposta erro = ErroResposta.erroPadrao(e.getMessage());
+
+        return ResponseEntity.status(erro.status()).body(erro);
+    }
+
+    // Exceções não tratadas
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErroResposta> handleRuntimeException(RuntimeException e){
+        ErroResposta erro = new ErroResposta(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Ocorreu um erro inesperado",
+                List.of()
+        );
+        return ResponseEntity.status(erro.status()).body(erro);
     }
 }
